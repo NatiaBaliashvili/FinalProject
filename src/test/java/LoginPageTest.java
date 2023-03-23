24a6d5480dadc0e9ce17ff90f4a61b4443d7be89
NatiaBaliashvili <gnatiabaliashvili@gmail.com>
2023-03-23 17:27:59 +0400

    ფინალური პროექტი

---
+++ src/test/java/LoginPageTest.java
@@ -0,0 +1,28 @@
+import StepObject.LoginPageSteps;
+import Utils.ChromeRunner;
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static DataObject.LoginPageData.email;
+import static DataObject.LoginPageData.password;
+import static com.codeborne.selenide.Selenide.sleep;
+
+public class LoginPageTest extends ChromeRunner {
+    LoginPageSteps loginPageSteps = new LoginPageSteps();
+
+    @Test
+    @Severity(SeverityLevel.MINOR)
+    public void UserNameOrPassword() {
+        loginPageSteps.MyAccount()
+                .EmailInput(email)
+                .PasswordInput(password)
+                .LoginBtn();
+        Assert.assertTrue(LoginPageSteps.loginError.is(Condition.visible), "პაროლის ველი");
+        sleep(5000);
+
+    }
+
+}
